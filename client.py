726e577d7ef63d217e5d067ce8f5c5748840ae80
João Guilherme <mine.slimrt@gmail.com>
2024-11-03 16:12:18 -0400

    adicionando comandos

--- client.py
+++ client.py
@@ -5,30 +5,50 @@ import threading
 from collections import deque
 import os
 
+msgs = deque()
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def show_messages():
+    clear()
+    for msg in msgs:
+        print(f"[{msg.name}]: {msg.text}")
+
 def send_message(stub, username):
     while True:
-        text = input(f"> {username}: ")
+        text = input(f"> ")
+
+        # Validações
+        if text == '':
+            show_messages()
+            continue
+
+        # Comandos
         if text.lower() == '/sair':
             break
+        if text == '/limpar':
+            msgs.clear()
+            clear()
+            continue
+
         message = chat_pb2.ChatMessage(name=username, text=text)
         stub.SendMessage(message)
 
 def receive_messages(stub):
-    msgs = deque()
     try:
         for chat_message in stub.ChatStream(chat_pb2.Empty()):
             msgs.append(chat_message)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            for msg in msgs:
-                print(f"[{msg.name}]: {msg.text}")
+            show_messages()
         
     except grpc.RpcError as e:
         print(f"Erro ao receber mensagens: {e}")
 
 def main():
     ip = input("Digite o IP do servidor de chat: ")
+    ip = f'{ip}:50051' if ip != '' else 'localhost:50051'
 
-    channel = grpc.insecure_channel(ip if ip != '' else 'localhost:50051')
+    channel = grpc.insecure_channel(ip)
     stub = chat_pb2_grpc.ChatServiceStub(channel)
 
     username = input("Digite seu nome de usuário: ")
